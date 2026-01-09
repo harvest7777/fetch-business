cc7d926c1e0366a7a5d91a5501a8672be6d6564b
harvest7777 <333harvest.moon333@gmail.com>
2026-01-09 10:44:32 -0800

    feat: created orders/ route and orders/health endpoint

--- business/config/urls.py
+++ business/config/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for business project.
+URL configuration for config project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
@@ -16,8 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("orders/", include("orders.urls")),
 ]
